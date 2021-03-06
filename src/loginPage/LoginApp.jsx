47b2d7de0aa12fe9e497ff4cea41b5152f84c76c
Ninad Parkar <ninadp@prdxn.com>
2021-03-07 00:51:47 +0530

    Savepoint

--- src/loginPage/LoginApp.jsx
+++ src/loginPage/LoginApp.jsx
@@ -1,19 +1,32 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import GlobalStyle from './asset/GlobalStyle';
 
 function LoginApp() {
+  const trimmer = useRef(null);
+  const container = useRef(null);
+
+  const signUpClicked = () => {
+    trimmer.current.classList.add("shift");
+    container.current.classList.add("switch");
+  }
+
+  const signInClicked = () => {
+    trimmer.current.classList.remove("shift");
+    container.current.classList.remove("switch");
+  }
+
   return (
     <>
       <GlobalStyle />
       <section className="auth">
         <div className="sign_in">
-          <button>Sign In</button>
+          <button onClick={signInClicked}>Sign In</button>
         </div>
         <div className="sign_up">
-          <button>Sign Up</button>
+          <button onClick={signUpClicked}>Sign Up</button>
         </div>
-        <div className="trimmer">
-          <div className="container">
+        <div className="trimmer" ref={trimmer}>
+          <div className="container" ref={container}>
             <div className="sign_in_form"><span>Sign In Form</span></div>
             <div className="sign_up_form"><span>Sign Up Form</span></div>
           </div>
