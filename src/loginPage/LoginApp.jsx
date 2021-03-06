0d06286555f707536803109ab0a77707c1ec1238
Ninad Parkar <ninadp@prdxn.com>
2021-03-07 01:56:02 +0530

    Savepoint

--- src/loginPage/LoginApp.jsx
+++ src/loginPage/LoginApp.jsx
@@ -1,5 +1,6 @@
 import React, { useRef } from 'react';
 import GlobalStyle from './asset/GlobalStyle';
+import styled from 'styled-components';
 
 function LoginApp() {
   const trimmer = useRef(null);
@@ -18,22 +19,73 @@ function LoginApp() {
   return (
     <>
       <GlobalStyle />
-      <section className="auth">
-        <div className="sign_in">
+      <Auth>
+        <SignIn>
           <button onClick={signInClicked}>Sign In</button>
-        </div>
-        <div className="sign_up">
+        </SignIn>
+        <SignUp>
           <button onClick={signUpClicked}>Sign Up</button>
-        </div>
-        <div className="trimmer" ref={trimmer}>
-          <div className="container" ref={container}>
-            <div className="sign_in_form"><span>Sign In Form</span></div>
-            <div className="sign_up_form"><span>Sign Up Form</span></div>
-          </div>
-        </div>
-      </section>
+        </SignUp>
+        <Trimmer ref={trimmer}>
+          <Container ref={container}>
+            <SignInForm><span>Sign In Form</span></SignInForm>
+            <SignUpForm><span>Sign Up Form</span></SignUpForm>
+          </Container>
+        </Trimmer>
+      </Auth>
     </>
   )
 }
+
+const Auth = styled.section`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  position: absolute;
+`;
+
+const SignIn = styled.div`
+  width: 50%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  background-color: darkblue;
+  justify-content: center;
+`;
+
+const SignUp = styled(SignIn)`
+  background-color: skyblue;
+`;
+
+const Trimmer = styled.div`
+  width: 50%;
+  height: 100%;
+  overflow: hidden;
+  position: absolute;
+  top: 0;
+  left: 0;
+  transform: translateX(0);
+  transition: transform .5s ease;
+`;
+
+const Container = styled.div`
+  width: 100vw;
+  height: 100%;
+  display: flex;
+  transform: translateX(0);
+  transition: transform 1s ease;
+`;
+
+const SignInForm = styled.div`
+  width: 50%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  background-color: white;
+  justify-content: center;
+`;
+
+const SignUpForm = styled(SignInForm)``;
+
 
 export default LoginApp
